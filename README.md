413a47c80f6f0edb5aea06c6c222dcfc885c613a
github-laziji <c_smtp@163.com>
2023-01-01 01:34:11 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,58 @@
 # AE表达式
 
-## 使用方式
+使用方式: AE导入所需jsx文件
+
+
+## trans.jsx
+### 弹性变化
+- 函数原型
+
+`elastic(freq, decay)`
+
+- 使用条件
+
+关键帧数>=2
+
+- 适用属性
+
+通用
+
+- 参数说明
+
+| 参数名 | 类型 | 默认值 | 说明 |
+| ---- | ---- | ---- | ---- |
+| `freq` | `Number` | `6`| 次数 |
+| `decay` | `Number` | `4`| 衰退率 |
+
+- 示例
 ```js
-exp = eval(footage("trans.jsx").sourceText);
+exp = eval(footage("trans.jsx").sourceText)
 exp.elastic(6, 4)
+```
+
+### 重力反弹
+- 函数原型
+
+`gravityRebound(e, g, freq)`
+
+- 使用条件
+
+关键帧数==2
+
+- 适用属性
+
+位置
+
+- 参数说明
+
+| 参数名 | 类型 | 默认值 | 说明 |
+| ---- | ---- | ---- | ---- |
+| `e` | `Number` | `0.7`| 弹性 |
+| `g` | `Number` | `1000`| 重力 |
+| `freq` | `Number` | `9`| 次数 |
+
+- 示例
+```js
+exp = eval(footage("trans.jsx").sourceText)
+exp.gravityRebound(0.7, 1000, 9)
 ```
--- src/trans.jsx
+++ src/trans.jsx
@@ -1,14 +1,22 @@
 (function () {
 
+    function validate(v, type, def) {
+        return typeof v === type ? v : def;
+    }
+
     /**
      * 弹性变化
-     * 条件: numKeys>=2
+     * 条件: 关键帧数>=2
+     * 类型: 通用
      * 
-     * @param freq 频率
+     * @param freq 次数
      * @param decay 衰退比
      * @returns 
      */
     function elastic(freq, decay) {
+        freq = validate(freq, 'number', 6);
+        decay = validate(decay, 'number', 4);
+
         if (numKeys < 2) {
             return value;
         }
@@ -19,13 +27,64 @@
         if (n <= 0) {
             return value;
         }
-        t = time - key(n).time;
-        amp = velocityAtTime(key(n).time - .001);
-        w = freq * Math.PI * 2;
+        let t = time - key(n).time;
+        let amp = velocityAtTime(key(n).time - .001);
+        let w = freq * Math.PI * 2;
         return value + amp * (Math.sin(t * w) / Math.exp(decay * t) / w);
     }
 
+    /**
+     * 重力反弹
+     * 条件: 关键帧数==2
+     * 类型: 位置
+     * 
+     * @param e 弹力 
+     * @param g 重力 
+     * @param freq 次数 
+     * @returns 
+     */
+    function gravityRebound(e, g, freq) {
+        e = validate(e, 'number', 0.7);
+        g = validate(g, 'number', 1000);
+        freq = validate(freq, 'number', 9);
+
+        if (numKeys != 2) {
+            return value;
+        }
+        let n = nearestKey(time).index;
+        if (key(n).time > time) {
+            n--;
+        }
+        if (n <= 0) {
+            return value;
+        }
+        let t = time - key(n).time;
+        let v = -velocityAtTime(key(n).time - .001) * e;
+        let vl = length(v);
+        let vu = 0;
+        if (value instanceof Array) {
+            vu = (vl > 0) ? normalize(v) : [0, 0, 0];
+        } else {
+            vu = (v < 0) ? -1 : 1;
+        }
+        let tCur = 0;
+        let segDur = 2 * vl / g;
+        let tNext = segDur; nb = 1;
+        while (tNext < t && nb <= freq) {
+            vl *= e; segDur *= e;
+            tCur = tNext;
+            tNext += segDur;
+            nb++
+        }
+        if (nb > freq) {
+            return value
+        }
+        let delta = t - tCur;
+        return value + vu * delta * (vl - g * delta / 2);
+    }
+
     return {
-        elastic
+        elastic,
+        gravityRebound
     };
 })()
